4f03debdc547a60ed0032aff5f792729697f9eac
christophegillis <chris.tim.gillis@gmail.com>
2019-04-08 11:45:52 +0200

    added rails routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+  root to: 'pages#home'
+
+  resources :bugs, only: [:show]
+  # resources :cocktails do
+  #     resources :doses, only: [:new, :create]
+  #   end
+
+  #   resources :doses, only: [:show, :edit, :update, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
