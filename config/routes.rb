f59f96d538fbf95cdefca4b4a10fa068c6678b7c
christophegillis <chris.tim.gillis@gmail.com>
2019-04-09 11:25:02 +0200

    added username column to user model

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resources :bugs, only: %i[index new show create destroy] do
-    resoures :fixes, only: %i[create edit destroy]
+    resources :fixes, only: %i[create edit destroy]
   end
 end
